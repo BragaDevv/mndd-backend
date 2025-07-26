86089b38408fec61e7119f6e90ad1e59c155c31b
BragaDevv <bragadevv@gmail.com>
2025-07-26 12:36:31 -0300

    Imagens OK

--- api/unsplash.ts
+++ api/unsplash.ts
@@ -1,19 +1,19 @@
 import { Request, Response } from "express";
 
 export default function unsplashHandler(req: Request, res: Response) {
-  try {
-    const temas = ["sky", "stars", "orange", "clouds", "sunset"];
-    const temaAleatorio = temas[Math.floor(Math.random() * temas.length)];
-    const seed = encodeURIComponent(temaAleatorio);
+    try {
+        const temas = ["blue-sky", "orange-sky", "starry-sky"];
+        const temaAleatorio = temas[Math.floor(Math.random() * temas.length)];
+        const seed = encodeURIComponent(temaAleatorio);
 
-    const url = `https://picsum.photos/seed/${seed}/1080/1920`;
+        const url = `https://picsum.photos/seed/${seed}/1080/1920`;
 
-    res.status(200).json({
-      url,
-      tema: temaAleatorio,
-    });
-  } catch (err) {
-    console.error("❌ Erro ao gerar URL do Picsum:", err);
-    res.status(500).json({ erro: "Erro ao gerar imagem aleatória" });
-  }
+        res.status(200).json({
+            url,
+            tema: temaAleatorio,
+        });
+    } catch (err) {
+        console.error("❌ Erro ao gerar URL do Picsum:", err);
+        res.status(500).json({ erro: "Erro ao gerar imagem aleatória" });
+    }
 }
